41a3409d8adc152f225753973947d13e92659173
Yasin Nazari <64962614+yasinnazari@users.noreply.github.com>
2025-07-08 11:31:33 +0330

    feat: structure validation logic into dedicated classes

--- src/validation/validation.py
+++ src/validation/validation.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, EmailStr, ValidationError, Field
 from typing import Optional
 
-class data_validation(BaseModel):
-   try:
-      message_text: str = Field(min_length=20, max_length=399)
-      sender_username: str = Field(min_length=5, max_length=30)
-   except ValidationError as err:
-      print(repr(err.errors()[0]['type']))
+class validate_send_msg(BaseModel):
+   message_text: str = Field(min_length=20, max_length=399)
+   sender_username: str = Field(min_length=5, max_length=30)
 
+class validate_del_msg(BaseModel):
+   id: int
